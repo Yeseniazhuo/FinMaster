e99aba5ea8beb3ce23ac0c7f166e36c2c7f35413
Yeseniazhuo <Yesenia_Zhuo@163.com>
2021-11-26 00:06:18 +0800

    Modify creat issue card

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-
-class User(models.Model):
-	Name = models.CharField(max_length=30)
-	Stock = models.CharField(max_length=30)
-	News = models.CharField(max_length=30)
-
 class Task(models.Model):
 	Issue = models.CharField(max_length=200)
 	Due = models.DateTimeField()
 	Tag = models.CharField()
+
+class User(models.Model):
+	UserName = models.CharField(max_length=30)
+	UserPassword = models.CharField(max_length=30)
+	Stock = models.CharField(max_length=30)
+	News = models.CharField(max_length=30)
+
+
