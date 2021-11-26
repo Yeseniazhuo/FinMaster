e7efee3734cbd8c4db7b57c605a02a3e2639c902
Yeseniazhuo <Yesenia_Zhuo@163.com>
2021-11-26 17:39:24 +0800

    Update models.py

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -4,6 +4,7 @@ class Task(models.Model):
 	Issue = models.CharField(max_length=200)
 	Due = models.DateTimeField()
 	Tag = models.CharField()
+	CompleteStatus = models.BooleanField(default=False)
 
 class User(models.Model):
 	UserName = models.CharField(max_length=30)
