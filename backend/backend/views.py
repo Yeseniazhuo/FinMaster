6785be17ff437fe6edf39d0f734678248a98536f
Yeseniazhuo <Yesenia_Zhuo@163.com>
2021-11-30 00:53:25 +0800

    change the small calendar into delay alert

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -13,7 +13,7 @@ from bokeh.models import BooleanFilter, CDSView, Select, Range1d, HoverTool
 from bokeh.palettes import Category20
 from bokeh.models.formatters import NumeralTickFormatter
 
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 
@@ -53,7 +53,6 @@ def plot_dashboard(selected_symbol):
     last_close = df.iloc[-1].close
 
     return script, div, last_high, last_close
-
 
 
 def dashboard_news(keyword):
@@ -259,48 +258,40 @@ def info_plot_3(selected_symbol):
     script, div = components(p)
     return script, div
 
-def decode_plot(buffer):
-    plot_data = buffer.getvalue()
-    imb=base64.b64encode(plot_data)
-    ims=imb.decode()
-    imd="data:image/png;base64," + ims
-    return imd
 
-def Draw_Progress():
-    unfinished_tasks = Task.objects.filter(due__lte=datetime.now(),due__gte=datetime.now()+\
-        timedelta(days=-7),complete_status=False)
-    finished_tasks = Task.objects.filter(due__lte=datetime.now(),due__gte=datetime.now()+\
-        timedelta(days=-7),complete_status=True)
-    plt.figure(figsize=(6,9)) 
-    labels = ['Finished Task','Unfinished Task'] #Label
-    sizes = [len(unfinished_tasks),len(finished_tasks)] #每块值
-    colors = ['yellowgreen','yellow'] 
-    plt.pie(sizes,labels=labels,colors=colors,shadow=True) 
-    plt.axis('equal')
-    buffer = BytesIO()
-    plt.savefig(buffer)
-    plt.close()
-    return buffer
-
+def progress_count():
+    unfinished_tasks = Task.objects.filter(due__lte=datetime.now(), due__gte=datetime.now() +
+                                           timedelta(days=-7), complete_status=False)
+    finished_tasks = Task.objects.filter(due__lte=datetime.now(), due__gte=datetime.now() +
+                                         timedelta(days=-7), complete_status=True)
+    unfinished = len(unfinished_tasks)
+    finished = len(finished_tasks)
+    return unfinished, finished
 
 
 def dashboard(request, task_id=None):
     """if not request.session.get('is_login', None):
         return redirect('/login/')"""
-    
+
+    unfinished, finished = progress_count()
+    print(unfinished, finished)
+
     selected_symbol = 'SPY'
     script, div, last_high, last_close = plot_dashboard(selected_symbol)
 
     keyword = 'stock'
-    news_title_1, news_title_2, news_title_3, news_content_1, news_content_2, news_content_3, news_url_1, news_url_2, news_url_3 = dashboard_news(keyword)
+    news_title_1, news_title_2, news_title_3, news_content_1, news_content_2, news_content_3, news_url_1, news_url_2, news_url_3 = dashboard_news(
+        keyword)
 
     start_time = this_monday()
     month = this_month()
-    calendar = mark_safe(small_calendar())
-    
-    #weekly Progress
-    buffer = Draw_Progress()
-    Progress_plot = decode_plot(buffer)
+    today = datetime.today().day
+
+    Delaytask = Task.objects.filter(due__lte=datetime.now(), complete_status=False)
+    if len(Delaytask) > 0:
+        delaynum = 'Warning!! '+str(len(Delaytask))+' Delayed task!'
+    else:
+        delaynum = ' Congratulation!! All Task finished on time!!'
 
     # create new issue
     instance = Task()
@@ -314,48 +305,50 @@ def dashboard(request, task_id=None):
         form.save()
         return HttpResponseRedirect(reverse('calendar'))
 
-    tasks = Task.objects.filter(due__lte=datetime.now()+timedelta(days=1),complete_status=False).order_by('due').reverse()
+    tasks = Task.objects.filter(due__lte=datetime.now(
+    )+timedelta(days=1), complete_status=False).order_by('due').reverse()
     if len(tasks) >= 3:
         lastest_task1 = tasks[0]
-        title1 =  lastest_task1.title
-        due1 =  lastest_task1.due
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
         lastest_task2 = tasks[1]
-        title2 =  lastest_task2.title
-        due2 =  lastest_task2.due
+        title2 = lastest_task2.title
+        due2 = lastest_task2.due
         lastest_task3 = tasks[2]
-        title3 =  lastest_task3.title
-        due3 =  lastest_task3.due
+        title3 = lastest_task3.title
+        due3 = lastest_task3.due
     elif len(tasks) == 2:
         lastest_task1 = tasks[0]
-        title1 =  lastest_task1.title
-        due1 =  lastest_task1.due
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
         lastest_task2 = tasks[1]
-        title2 =  lastest_task2.title
-        due2 =  lastest_task2.due
-        title3 =  'No Third task'
-        due3 =  'No Third task'
+        title2 = lastest_task2.title
+        due2 = lastest_task2.due
+        title3 = 'No Third task'
+        due3 = 'No Third task'
     elif len(tasks) == 1:
         lastest_task1 = tasks[0]
-        title1 =  lastest_task1.title
-        due1 =  lastest_task1.due
-        title2 =  'No Second task'
-        due2 =  'No Second task'
-        title3 =  'No Third task'
-        due3 =  'No Third task'
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
+        title2 = 'No Second task'
+        due2 = 'No Second task'
+        title3 = 'No Third task'
+        due3 = 'No Third task'
     elif len(tasks) == 0:
-        title1 =  'No First task'
-        due1 =  'No First task'
-        title2 =  'No Second task'
-        due2 =  'No Second task'
-        title3 =  'No Third task'
-        due3 =  'No Third task'
+        title1 = 'No First task'
+        due1 = 'No First task'
+        title2 = 'No Second task'
+        due2 = 'No Second task'
+        title3 = 'No Third task'
+        due3 = 'No Third task'
+
     return render(request, 'Dashboard.html', locals())
 
 
 def info(request):
     """if not request.session.get('is_login', None):
         return redirect('/login/')"""
-    
+
     keyword = 'AAPL AND stock'
 
     selected_symbol = 'AAPL'
